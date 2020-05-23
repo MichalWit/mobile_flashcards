23cbeeea7214c8211b512a23104daefe7699fc46
Michal <michalwit111@gmail.com>
2020-05-23 11:42:22 +0200

    feat: add two tabs: Decks and Add deck

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Decks from './components/Decks'
+import AddDeck from './components/AddDeck'
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Decks" component={Decks} />
+        <Tab.Screen name="New Deck" component={AddDeck} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
+// TODO
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ components/AddDeck.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function AddDeck() {
+
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Add a new deck!</Text>
+      </View>
+    );
+}
+
+export default AddDeck
